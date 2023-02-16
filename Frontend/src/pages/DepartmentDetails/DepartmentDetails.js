6667735e17ee4787b13ba29a52ddbd023652c97a
Habeeb8136 <habeeb041099@gmail.com>
2023-02-16 14:02:44 +0530

    details page

--- Frontend/src/pages/DepartmentDetails/DepartmentDetails.js
+++ Frontend/src/pages/DepartmentDetails/DepartmentDetails.js
@@ -1,11 +1,65 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './DepartmentDetails.css'
+import employees from '../../Assets/MOCK_DATA.json';
+import EmployeeCard from '../../components/EmployeeCard/EmployeeCard'
+import { updateEmployee } from '../../features/details';
 const DepartmentDetails = () => {
+    const department=useSelector(state=>state.details.department)
+    const [departmenttEmployees, setDepartmenttEmployees] = useState();
+    const dispatch=useDispatch()
+
+    useEffect(() => {
+        const filteredData=employees.filter((emp)=>{
+            return emp.department.name===department.name
+        })
+        
+        setDepartmenttEmployees(filteredData)
+    }, [department.name]);
+    
+    console.log(departmenttEmployees)
+    
+    function setState(head){
+        const filteredData=employees.filter((emp)=>{
+            return emp.name===head
+        })
+        dispatch(updateEmployee(...filteredData))
+            console.log(...filteredData)
+
+    }
+
+
+
     return (
-        <div>
-            <h1>Dipartment details</h1>
+        <>
+        <div className='department-details-container'>
+            <div className='details-left'>
+                <h1>Department of {department.name}</h1>
+                <p>Founded : {department.founded}</p>
+                <br></br>
+                <br></br>
+                <h3>{department.description}</h3>
+                <br></br>
+                
+                <h4 onClick={()=>setState(department.head)}>Head Of Department: <Link to='/employee-detail'><span >{department.head}</span></Link></h4>
+                
+            </div>
+            <div className='details-right'>
+                <img src={department.img} alt=""></img>
+            </div>
+
         </div>
-    );
+
+        <div className='list'>
+            <h3>employees in department</h3>
+            {departmenttEmployees?.map((employee)=>{
+                return <EmployeeCard employee={employee} key={employee.id}/>
+            })}
+        </div>
+
+            </>
+            );
 }
 
 export default DepartmentDetails;
--- Frontend/src/pages/EmployeeDetails/EmployeeDetails.js
+++ Frontend/src/pages/EmployeeDetails/EmployeeDetails.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {BsFillTelephoneForwardFill} from 'react-icons/bs'
+import employees from '../../Assets/MOCK_DATA.json'
+import departments from '../../Assets/MOCK DEPARTMENTS.json'
+import EmployeeCard from '../../components/EmployeeCard/EmployeeCard';
+import './EmployeeDetails.css'
+import { updateDepartment  } from '../../features/details';
 
 const EmployeeDetails = () => {
+    const [DepartmentHead, setDepartmentHead] = useState();
+    const employee=useSelector(state=>state.details.employee)
+    console.log(employee)
+    const dispatch=useDispatch()
+
+    useEffect(() => {
+        const filteredData=employees?.filter((emp)=>{
+            return emp.department.name===employee.department.name
+            && emp.head.status===true
+        })
+        
+        setDepartmentHead(filteredData)
+    }, [employee.department.name]);
+    console.log(DepartmentHead)
+
+    function setState(head){
+        const filteredData=departments.filter((departments)=>{
+            return departments.name===head
+        })
+        dispatch(updateDepartment(...filteredData))
+            console.log(...filteredData)
+
+    }
+    
+
     return (
-        <div>
-            <h1>Employees Detailspage</h1>
+        
+        <div className='employee-details-container'>
+            <div className='details-left'>
+                <h1>{employee?.name}</h1>
+                <h4 className='head-indicator'>{employee?.head.status ?"Head of Department":null}</h4>
+                <p><BsFillTelephoneForwardFill/> {employee?.phone}</p>
+                
+                <h4>Age <strong>{employee?.age}</strong></h4>
+                <br></br>
+                <p>{employee?.descrition}</p>
+                <h4 onClick={()=>setState(employee?.department.name)}>Department :
+                 <Link to='/department-detail'> {employee?.department.name}</Link>
+                 </h4>
+                <br></br>
+                {employee?.head.status ?null:
+                
+                
+                <h3>Report to HOD:<br></br><br></br> <span style={{color:'red'}}>
+                    {DepartmentHead?.map((employee)=>
+                        <Link to='employee-detail'><EmployeeCard employee={employee}/></Link>
+                    ) }
+                 </span></h3>
+                
+
+
+                }
+
+
+            </div>
+            <div className={employee?.head.status ?'employee-right-head':'employee-right'}>
+                <img src={employee?.img} alt=""></img>
+            </div>
+
         </div>
     );
 }
