b009d2b70050b6f360b218571b1900a0230d9434
Habeeb8136 <habeeb041099@gmail.com>
2023-02-16 14:00:41 +0530

    footer

---
+++ Frontend/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <Container>
+                <div>v1.3</div>
+            </Container>
+        </div>
+    );
+}
+
+export default Footer;
