860a43e631676669f1c8ab7e0f1a93c66b2da030
Habeeb8136 <habeeb041099@gmail.com>
2023-02-16 14:02:06 +0530

    dashboard page

--- Frontend/src/pages/Dashboard/Dashboard.js
+++ Frontend/src/pages/Dashboard/Dashboard.js
@@ -1,21 +1,54 @@
-import React, { useState } from 'react';
-import { Link, Navigate} from 'react-router-dom'
+import React from 'react';
+import './Dashboard.css'
+import Departments from '../Departments/Departments';
+import Employees from '../Employees/Employees';
+import DepartmentHeads from '../DepartmentHeads/DepartmentHeads';
 
-const Dashboard = ({userLogged ,setUserLogged }) => {
-    const [redirectToDashboard, setRedirectToDashboard] = useState(false);
-    function handleLogout(){
-        setRedirectToDashboard(true)
-    }
-    if(redirectToDashboard){
-        return <Navigate replace to='/Signin'/>
-    }
+
+const Dashboard = () => {
+    
         
         
     return (
-        <div>
-            {/* <Link to='/Signin'> */}
-            <h1 onClick={()=>handleLogout()}>dashboard</h1>
-            {/* </Link> */}
+        <div className='dash'>
+
+                <div className='selector-department'>
+                    <Departments />
+                </div>
+
+                <div className='selector-employees'>
+                    <Employees />
+                </div>
+
+                <div className='selector-heads'>
+                    <DepartmentHeads />
+                </div>
+            
+                {/* <div className='selector-department'>
+                <Link to='departments'>
+                <div className='dept'>
+                Departments
+                </div>    
+                </Link>
+                </div>
+
+                <div className='selector-employees'>
+                <Link to='employees'>
+                <div className='dept'>
+                Employees
+                </div>
+                </Link>
+                </div>
+
+                <div className='selector-heads'>
+                <Link to='heads'>
+                <div className='dept'>
+                Department Heads
+                </div>
+                </Link>
+                </div> */}
+          
+           
         </div>
     );
 }
