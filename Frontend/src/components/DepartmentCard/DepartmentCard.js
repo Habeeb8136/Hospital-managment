dc11203bae5b08521296a69f2e9482d2ffc0370d
Habeeb8136 <habeeb041099@gmail.com>
2023-02-16 14:00:04 +0530

    Department card

---
+++ Frontend/src/components/DepartmentCard/DepartmentCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { updateDepartment } from '../../features/details';
+import {Link} from 'react-router-dom'
+import './DepartmentCard.css'
+
+const DepartmentCard = ({department}) => {
+
+    const dispatch=useDispatch()
+
+    function handleClick(department){
+        dispatch(updateDepartment(department))
+        console.log(department.name)
+    }
+
+    return (
+        
+        <Link to='/department-detail'>
+         <div className='department-card' onClick={()=>handleClick(department)}>
+           <div className='card-left'>
+                <p>Department Name: <strong>{department.name}</strong></p>
+            </div>
+
+            <div className='card-right'>
+                <img src={department.img} alt="profilepic"></img>
+            </div>
+        </div>
+        </Link>
+        
+    );
+}
+
+export default DepartmentCard;
